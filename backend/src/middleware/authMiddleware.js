2693570031b1f3e2251eeda4cbb2acc47488ce5e
Tito-Osemobor <titoosemobor@gmail.com>
2025-03-12 01:57:53 -0400

    Remove redundant variable

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -8,8 +8,7 @@ const authenticate = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach user data to the request
+    req.user = jwt.verify(token, process.env.JWT_SECRET); // Attach user data to the request
     next();
   } catch (error) {
     return res.status(403).json({success: false, message: "Unauthorized: Invalid token"});
