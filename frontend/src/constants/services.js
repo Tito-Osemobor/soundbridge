887e6969289b396a474e88be90ce3569cc76b7a9
Tito-Osemobor <titoosemobor@gmail.com>
2025-04-02 00:21:28 -0400

    Create ENUMS for Supported platforms

--- frontend/src/constants/services.js
+++ frontend/src/constants/services.js
@@ -1,25 +1,26 @@
 import { FaSpotify } from "react-icons/fa";
 import { SiYoutubemusic } from "react-icons/si";
 
-// 🔹 Define supported platforms in one place
-export const SUPPORTED_SERVICES = [
-  {
+export const PLATFORM_IDS = {
+  SPOTIFY: "SPOTIFY",
+  YOUTUBE_MUSIC: "YOUTUBE_MUSIC",
+};
+
+export const SUPPORTED_PLATFORMS = new Map([
+  [PLATFORM_IDS.SPOTIFY, {
     id: "SPOTIFY",
     name: "Spotify",
     color: "bg-green-500",
     hoverColor: "hover:bg-green-600",
     icon: (className) => <FaSpotify className={className} />,
-  },
-  {
+  }],
+  [PLATFORM_IDS.YOUTUBE_MUSIC, {
     id: "YOUTUBE_MUSIC",
     name: "YouTube Music",
     color: "bg-red-500",
     hoverColor: "hover:bg-red-600",
-    icon: (className) => <SiYoutubemusic className= {className} />,
-  },
-];
+    icon: (className) => <SiYoutubemusic className={className} />
+  }]
+]);
 
-// 🔹 Helper function to get service details by name
-export const getServiceByName = (serviceName) => {
-  return SUPPORTED_SERVICES.find(service => service.name === serviceName);
-};
+export const getPlatformById = (platformId) => SUPPORTED_PLATFORMS.get(platformId);
