f004c7ffee8245961edb0feaa653f11c96f8665a
Tito-Osemobor <titoosemobor@gmail.com>
2025-04-02 00:28:28 -0400

    Redirect route if user is set

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,17 +1,37 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Button from "@/components/Button";
+import {useAuth} from "@/context/AuthContext";
+import {useRouter} from "next/router";
 
-export default function Home() {
+const Home = () => {
+  const {user} = useAuth();
+  const router = useRouter();
+
+  console.log(user);
+  const handleClick = () => {
+    if (user) {
+      router.push("/hub");
+    } else {
+      router.push("/login");
+    }
+  }
+
   return (
     <div className="flex flex-col min-h-screen select-none">
-      <Navbar />
-      <main className="flex flex-col items-center justify-center flex-grow text-center">
-        <h1 className="text-8xl font-bold">SoundBridge</h1>
-        <p className="text-3xl mt-2 text-gray-600">
-          Seamlessly transfer your music across platforms
-        </p>
+      <Navbar/>
+      <main className="flex flex-col items-center justify-center flex-grow text-center gap-3">
+        <div>
+          <h1 className="text-8xl font-bold">SoundBridge</h1>
+          <p className="text-3xl mt-2 text-gray-600">
+            Seamlessly transfer your music across platforms
+          </p>
+        </div>
+        <Button className={`text-2xl py-3 px-5`} children={`Get Started`} onClick={handleClick}/>
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
+
+export default Home;
