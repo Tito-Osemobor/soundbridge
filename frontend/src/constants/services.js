65f0cdb26872a6856c42f22ea29cfe17328c165f
Tito-Osemobor <titoosemobor@gmail.com>
2025-04-06 01:33:07 -0400

    Update platform color styles in services.js for improved accessibility and visual consistency

--- frontend/src/constants/services.js
+++ frontend/src/constants/services.js
@@ -10,15 +10,19 @@ export const SUPPORTED_PLATFORMS = new Map([
   [PLATFORM_IDS.SPOTIFY, {
     id: "SPOTIFY",
     name: "Spotify",
-    color: "bg-green-500",
-    hoverColor: "hover:bg-green-600",
+    color: "text-white",
+    backGroundColor: "bg-green-500",
+    mainColor: "text-green-500",
+    hoverColor: "hover:text-green-600",
     icon: (className) => <FaSpotify className={className} />,
   }],
   [PLATFORM_IDS.YOUTUBE_MUSIC, {
     id: "YOUTUBE_MUSIC",
     name: "YouTube Music",
-    color: "bg-red-500",
-    hoverColor: "hover:bg-red-600",
+    color: "text-red-500",
+    backGroundColor: "bg-white",
+    mainColor: "text-red-500",
+    hoverColor: "hover:text-red-600",
     icon: (className) => <SiYoutubemusic className={className} />
   }]
 ]);
