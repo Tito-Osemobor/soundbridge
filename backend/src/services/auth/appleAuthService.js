67e5d331941b352f54cb57deb899b057a9b37dd7
Tito-Osemobor <titoosemobor@gmail.com>
2025-04-18 16:39:33 -0400

    Move appleService.js for improved module organization

--- backend/src/services/auth/appleAuthService.js
+++
@@ -1,9 +0,0 @@
-const appleLoginService = (req, res) => {
-  res.send("Apple Music authentication will be implemented soon.");
-};
-
-const appleCallbackService = (req, res) => {
-  res.send("Apple Music OAuth callback handling coming soon.");
-};
-
-module.exports = { appleLoginService, appleCallbackService };
